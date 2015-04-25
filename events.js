7cf10aae62f73b39aba2ed57c16fdc2389ae7dda
Micheal Winger <mike.winger87@gmail.com>
2015-04-25 11:41:20 -0400

    old management of key events, no longer needed

--- events.js
+++ events.js
@@ -21,17 +21,3 @@ document.onkeydown=function(e){
   }
   if (pacman.leftState || pacman.rightState || pacman.upState || pacman.downState) pacman.queuedMovement = true;
 }
-
-/*document.onkeyup=function(e){
-  var evt = e ? e:event;
-  var keyCode = evt.keyCode;
-
-  // switch (keyCode)
-  // {
-  //   case 37: pacman.leftState  = false; break;
-  //   case 39: pacman.rightState = false; break;
-  //   case 38: pacman.upState    = false; break;
-  //   case 40: pacman.downState  = false; break;
-  // }
-  if (pacman.leftState || pacman.rightState || pacman.upState || pacman.downState) pacman.queuedMovement = true;
-}*/
